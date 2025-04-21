f2022acdfca812ec0c3be9864dfa5a8b5182195d
Alexy <alexy.vda@outlook.fr>
2025-04-22 01:53:04 +0200

    add more style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col">
+    <div className="items-center py-20 justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)] flex flex-col">
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
-      <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
+      <h1 className="text-[210px] font-bold row-start-2 uppercase text-center">
+        selected Works
+      </h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
       <h1 className="text-9xl font-bold row-start-2">HELLO</h1>
